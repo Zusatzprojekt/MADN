a0eab62225cfac170bbea99fd96f7606244ac7e6
BReep <67698695+Official-BReep@users.noreply.github.com>
2024-11-15 09:15:27 +0100

    abcd

--- src/Spieler.java
+++ src/Spieler.java
@@ -1,8 +1,13 @@
 public class Spieler {
+    private Wuerfel wuerfel;
+    private Spielfeld spielfeld;
     public Spieler(){
-
+        wuerfel = new Wuerfel();
+        spielfeld = new Spielfeld();
     }
-    public void move(){
-
+    public int move(){
+        int roll = wuerfel.wuerfeln();
+        spielfeld.get_positions();
+        return roll;
     }
 }
