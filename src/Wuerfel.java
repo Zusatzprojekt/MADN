bd4012e319cb7d19d9a2dfd019af5aa92952ed76
mzig4 <187798744+mzig4@users.noreply.github.com>
2024-11-15 09:16:45 +0100

    Wuerfel aktualisieren

--- src/Wuerfel.java
+++ src/Wuerfel.java
@@ -1,17 +1,17 @@
-
 /**
  * Beschreiben Sie hier die Klasse Würfel.
- * 
- * @author (Ihr Name) 
+ *
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
+
 import java.util.Random;
 
 public class Wuerfel
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int augenanzahl;
-    private Random rand = new Random();
+//    private int augenanzahl;
+    private final Random rand = new Random();
 
     /**
      * Konstruktor für Objekte der Klasse Würfel
@@ -23,8 +23,7 @@ public class Wuerfel
 
     public int wuerfeln()
     {
-        // tragen Sie hier den Code ein
-        int roll = rand.nextInt(1, 7);
-        return roll;
+        // Gibt eine Zahl zwischen 1 und 6 zurück
+        return rand.nextInt(1, 7);
     }
 }
