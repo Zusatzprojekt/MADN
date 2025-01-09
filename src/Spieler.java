7a5cc5beff95968529a2d30a53b63781ab4c3d29
BReep <67698695+Official-BReep@users.noreply.github.com>
2025-01-09 13:15:17 +0100

    wähle figur

--- src/Spieler.java
+++ src/Spieler.java
@@ -1,19 +1,31 @@
 import java.io.Console;
+import java.util.Scanner;
 
 public class Spieler {
     private Wuerfel wuerfel;
     private Spielfeld spielfeld;
+    private Scanner scanner;
 
     public Spieler() {
         wuerfel = new Wuerfel();
         spielfeld = new Spielfeld();
+        scanner = new Scanner(System.in);
     }
 
     public void move() {
         //int roll = wuerfel.wuerfeln();
         int roll = 1; //testen
-        int[] result = spielfeld.get_positions("red");
-        int werfen_target = spielfeld.pruefe_werfen("red", roll);
+        int[] positionen = spielfeld.get_positions("red");
+        System.out.println("Welche Figur soll bewegt werden, gebe bitte das jeweilige Feld an");
+        int auswahl = scanner.nextInt();
+        int figur=0;
+        for(int position: positionen){
+            if(position == auswahl){
+                figur = auswahl;
+            }
+        }
+        //int index_figur =
+        int werfen_target = spielfeld.pruefe_werfen("red",figur, roll);
         if (werfen_target != 0) {
             spielfeld.werfen(werfen_target, roll);
         }
--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -12,10 +12,11 @@ public class Spielfeld {
      */
     public Spielfeld() {
         // Instanzvariable initialisieren
-        path_red = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
-        path_blue = new int[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        path_yellow = new int[]{21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        path_green = new int[]{31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+        //Bei den letzen vier Zahlen handelt es sich jedes mal um die zielfelder
+        path_red = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,496,497,498,499};
+        path_blue = new int[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,196,197,198,199};
+        path_yellow = new int[]{21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,296,297,298,299};
+        path_green = new int[]{31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,396,397,398,399};
         positions = new HashMap<String, int[]>();
         positions.put("red", new int[]{5, 11});
         positions.put("blue", new int[]{12, 3});
@@ -27,7 +28,7 @@ public class Spielfeld {
         return positions.get(farbe);
     }
 
-    public int pruefe_werfen(String farbe, int augenzahl) {
+    public int pruefe_werfen(String farbe,int figur, int augenzahl) {
         int[] positions_farbe = positions.get(farbe);
         String[] farben = {"red", "blue", "yellow", "green"};
         for (String get_farbe : farben) {
