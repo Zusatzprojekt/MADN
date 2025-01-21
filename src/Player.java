7ef24521c19fff5f343a516c5c41bd65bd551cad
mzig4 <187798744+mzig4@users.noreply.github.com>
2025-01-21 16:10:12 +0100

    Add first methods

---
+++ src/Player.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public class Player {
+    private Figure[] figures;
+    private int playerID;
+    private int startField;
+    private Color color;
+
+    public Player(Figure[] figures, int playerID, int startField, Color color) {
+        this.figures = figures;
+        this.playerID = playerID;
+        this.startField = startField;
+        this.color = color;
+    }
+
+    public Figure[] canFigureMove(int rolledValue){
+        // TODO: Implementation
+        return null;
+    }
+
+    public boolean moveFigure(Figure movableFigure, int rolledValue){
+        // TODO: Implementation
+        return false;
+    }
+}
