738cc887a1708ac0fcc167f349afd214f7c32892
nicog01 <nico.graf2001@gmail.com>
2025-02-20 11:13:00 +0100

    Add startmenu

---
+++ src/main/java/com/github/zusatzprojekt/madn/ui/controllers/StartmenuController.java
@@ -0,0 +1,9 @@
+package com.github.zusatzprojekt.madn.ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartmenuController {
+    @FXML
+    public Button playButton;
+}
