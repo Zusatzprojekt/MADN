98f2763a51128b992f064895c248d9b72beea3e6
mzig4 <187798744+mzig4@users.noreply.github.com>
2025-02-20 11:20:02 +0100

    Add initialize to GameBoardController

--- src/main/java/com/github/zusatzprojekt/madn/ui/controllers/GameBoardController.java
+++ src/main/java/com/github/zusatzprojekt/madn/ui/controllers/GameBoardController.java
@@ -1,10 +1,30 @@
 package com.github.zusatzprojekt.madn.ui.controllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
 
-public class GameBoardController {
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class GameBoardController implements Initializable {
+    private Point2D[] runway = new Point2D[40];
+
     @FXML
     public Pane playerPane;
+    @FXML
+    public Polygon waypoints;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        double[] points = waypoints.getPoints().toArray(double[].class);
+
+//        System.out.println(Arrays.toString(points));
+    }
 }
