d54082085651019489a80fc5753270074c580525
xBlacksky <t.hauslohner@gmail.com>
2025-02-27 11:17:02 +0100

    Adjusted endscreen

--- src/main/java/com/github/zusatzprojekt/madn/Main.java
+++ src/main/java/com/github/zusatzprojekt/madn/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/parts/endscreen.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
