11c1823ab5951f4444174c8b047a15773c685f69
xBlacksky <t.hauslohner@gmail.com>
2025-06-27 12:11:24 +0200

    Add IDs in end-view.fxml and variables in EndViewController.java

--- src/main/java/com/github/zusatzprojekt/madn/ui/controller/EndViewController.java
+++ src/main/java/com/github/zusatzprojekt/madn/ui/controller/EndViewController.java
@@ -4,11 +4,15 @@ import com.github.zusatzprojekt.madn.interfaces.FxmlValueReceiver;
 import com.github.zusatzprojekt.madn.ui.AppManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.util.Map;
 
 public class EndViewController implements FxmlValueReceiver {
     private Map<String, Object> lastActivePlayers;
+
+    @FXML
+    private Label firstPlace, secondPlace, thirdPlace, fourthPlace;
 
     @FXML
     private void onPlayAgain(ActionEvent actionEvent) {
